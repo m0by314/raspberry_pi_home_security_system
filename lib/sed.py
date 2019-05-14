1a227e1cf5d5cf7f9fb0b58df6e2ece74538d622
adri31500 <43465735+adri31500@users.noreply.github.com>
2019-05-14 14:54:00 +0200

    Update sed.py

--- lib/sed.py
+++ lib/sed.py
@@ -1,9 +1,25 @@
-#! /usr/bin/python3
-# like sed for python 
+#!/usr/bin/python3
 
+import re
+import sys
+import shutil
 
-
-def sed(file):
+def sed(file,motif,remplacement):
+        regex = re.compile(motif)
+        tmpfile = file + '.tmp'
+ 
+        fin = open(file,'r')
+        fout = open(tmpfile,'w')
         
-    
-sed('test_sed.txt')
+        sys.stdout = fout   # Change STDOUT channel
+        
+        for line in fin:
+                if (regex.match(line)):
+                        line = re.sub(motif,remplacement,line)
+                        sys.stdout.write(str(line))
+                else:
+                        sys.stdout.write(str(line))
+        fin.close()
+        fout.close()
+        
+        shutil.move(tmpfile,file)
