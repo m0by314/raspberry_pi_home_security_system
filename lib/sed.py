bfe6d581f866fa7eaadfe2f0003f75a0e884a7de
adrien <adrien>
2019-05-14 21:22:23 +0200

    sed for python

--- lib/sed.py
+++ lib/sed.py
@@ -7,6 +7,7 @@ import shutil
 def sed(file,motif,remplacement):
         regex = re.compile(motif)
         tmpfile = file + '.tmp'
+        err = 0
  
         fin = open(file,'r')
         fout = open(tmpfile,'w')
@@ -14,12 +15,23 @@ def sed(file,motif,remplacement):
         sys.stdout = fout   # Change STDOUT channel
         
         for line in fin:
-                if (regex.match(line)):
-                        line = re.sub(motif,remplacement,line)
-                        sys.stdout.write(str(line))
-                else:
-                        sys.stdout.write(str(line))
+            line = re.sub(motif,remplacement,line)
+            try :
+                sys.stdout.write(str(line))
+            except Exception as E :
+                raise E
+                err = 1
+        
         fin.close()
         fout.close()
         
-        shutil.move(tmpfile,file)
+        if ( err != 1 ) :
+            shutil.move(tmpfile,file)
+        
+        
+if __name__ == '__main__':
+    if (len(sys.argv) != 4):
+        print('Usage:')
+        print('\t' + sys.argv[0] + ' file motif remplacement')
+    else:   
+        sed(sys.argv[1],sys.argv[2],sys.argv[3])
