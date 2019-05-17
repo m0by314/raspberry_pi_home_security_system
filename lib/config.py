845d94322c0541ab6dd278e47e7add4230107ae7
adri31500 <43465735+adri31500@users.noreply.github.com>
2019-05-18 01:52:03 +0200

    Update config.py

--- lib/config.py
+++ lib/config.py
@@ -2,12 +2,10 @@
 # Variable definition file
 
 bot_id      = 'Your_token_id'
-
+video_time  = 20
 tmp_path    = 'tmp/video/'
-video_name       = 'intrusion_'
-selfie_name      = 'selfie.jpeg'
-video_time      = 20
-
+video_name  = 'intrusion_'
+selfie_name = 'selfie.jpeg'
 video_path  = tmp_path + video_name
 selfie_path = tmp_path + selfie_name
 
