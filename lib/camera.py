09fd098c6f8452ffe28e8a8d5592ebdd09b7dc77
m0by314 <43465735+m0by314@users.noreply.github.com>
2019-05-28 18:33:29 +0200

    test

--- lib/camera.py
+++ lib/camera.py
@@ -7,45 +7,47 @@ import time
 from picamera  import PiCamera
 from .config    import video_path, selfie_path, video_time
 
-camera = PiCamera()
+class Camera:
+    def __init__(self):
+        sefl.camera = PiCamera()
 
-def start_record():
-    """
-    start record during 20s,
-    return video name at mp4 format or error if convert fail
-    """
+    def start_record(self):
+        """
+        start record during 20s,
+        return video name at mp4 format or error if convert fail
+        """
 
-    timestr = time.strftime("%H%M%S-%Y%m%d")
-    file = video_path + timestr
-    file_h264 = file + '.h264'
-    file_mp4  = file + '.mp4'
+        timestr = time.strftime("%H%M%S-%Y%m%d")
+        file = video_path + timestr
+        file_h264 = file + '.h264'
+        file_mp4  = file + '.mp4'
 
-    camera.start_recording(file_h264)
-    time.sleep(video_time)
-    camera.stop_recording()
+        sefl.camera.start_recording(file_h264)
+        time.sleep(video_time)
+        sefl.camera.stop_recording()
 
-    error = _convert_h264_to_mp4(file_h264, file)
-    if error == 0:
-        return { 0 : file_mp4 }
-    else:
-        return { 1 : error }
+        error = _convert_h264_to_mp4(file_h264, file)
+        if error == 0:
+            return { 0 : file_mp4 }
+        else:
+            return { 1 : error }
 
 
-def _convert_h264_to_mp4(file_h264, file):
-    """
-    convert format h264 in mp4
-    return error message if convertion is in fail
-    """
+    def _convert_h264_to_mp4(file_h264, file):
+        """
+        convert format h264 in mp4
+        return error message if convertion is in fail
+        """
 
-    command = "MP4Box -add {} {}.mp4".format(file_h264, file)
-    try:
-      output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
-    except subprocess.CalledProcessError as error:
-      err = 'FAIL:\ncmd:{}\noutput:{}'.format(error.cmd, error.output)
-      return err
-    else:
-      return 0
+        command = "MP4Box -add {} {}.mp4".format(file_h264, file)
+        try:
+            output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+        except subprocess.CalledProcessError as error:
+            err = 'FAIL:\ncmd:{}\noutput:{}'.format(error.cmd, error.output)
+            return err
+        else:
+            return 0
 
-def selfie():
-    camera.capture(selfie_path)
-    return selfie_path
+    def selfie():
+        sefl.camera.capture(selfie_path)
+        return selfie_path
