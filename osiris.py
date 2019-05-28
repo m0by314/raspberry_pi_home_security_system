0a2aa9796d499bca999ba6c1531a1f4780f130e6
adrien <adrien>
2019-05-28 18:10:43 +0100

    test2

--- osiris.py
+++ osiris.py
@@ -4,9 +4,9 @@ import time
 from gpiozero import MotionSensor
 
 from lib.telebot import chat_listen, send_video, handle, bot
-import lib.camera as camera
+from lib.camera import Camera
 
-c = camera.()
+c = Camera()
 pir = MotionSensor(4)
 
 bot.message_loop(handle) # start bot , wait commands
