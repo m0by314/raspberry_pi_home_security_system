99a471ef14c05a0e406086f44a589faa93bb2178
m0by314 <43465735+m0by314@users.noreply.github.com>
2019-05-30 16:57:13 +0200

    Update osiris.py

--- osiris.py
+++ osiris.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 
 import time
-from gpiozero import MotionSensor
+#from gpiozero import MotionSensor
 
-from lib.telebot import chat_listen, send_video, handle, bot
-from lib.camera import Camera
+#from lib.telebot import chat_listen, send_video, handle, bot
+#from lib.camera import Camera
+from lib.config import chat_id
+#c = Camera()
+#pir = MotionSensor(4)
+import lib.telepot
 
-c = Camera()
-pir = MotionSensor(4)
-
-bot.message_loop(handle) # start bot , wait commands
+bot = Telepot(chat_id)
 
 print ('I am listening ...')
-
+"""
 try:
     while True:
-        for chat_id, is_listening in chat_listen.items():
             if is_listening:
-                if pir.motion_detected:
-                    print ("Motion")
-                    c.send_video(c.start_record())
+               # if pir.motion_detected:
+                   # print ("Motion")
+                   # c.send_video(c.start_record())
                 else :
                     time.sleep(1)
             else:
                 time.sleep(1)
 
 except KeyboardInterrupt:
-    c.camera.close()
+""""
