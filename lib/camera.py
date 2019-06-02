ca1eb9fb09aea27a977680cd4a767899f604b5c2
m0by314 <43465735+m0by314@users.noreply.github.com>
2019-06-02 21:44:55 +0200

    Update camera.py

--- lib/camera.py
+++ lib/camera.py
@@ -8,38 +8,36 @@ from picamera  import PiCamera
 from .config    import video_path, selfie_path, video_time
 
 class Camera:
-    def __init__(self):
-        sefl.camera = PiCamera()
-
+    def __init__(self,video_path,video_time=20):
+        self.camera     = PiCamera()
+        self.file       = video_path + time.strftime("%H%M%S-%Y%m%d")
+        self.file_h264  = self.file + '.h264'
+        self.file_mp4   = self.file + '.mp4'
+        self.video_time = video_time
+        
     def start_record(self):
         """
         start record during 20s,
         return video name at mp4 format or error if convert fail
         """
+        self.camera.start_recording(self.file_h264)
+        time.sleep(self.video_time)
+        self.camera.stop_recording()
 
-        timestr = time.strftime("%H%M%S-%Y%m%d")
-        file = video_path + timestr
-        file_h264 = file + '.h264'
-        file_mp4  = file + '.mp4'
-
-        sefl.camera.start_recording(file_h264)
-        time.sleep(video_time)
-        sefl.camera.stop_recording()
-
-        error = _convert_h264_to_mp4(file_h264, file)
+        error = self._convert_h264_to_mp4(self.file_h264, self.file_mp4)
         if error == 0:
-            return { 0 : file_mp4 }
+            return { 0 : self.file_mp4 }
         else:
             return { 1 : error }
 
 
-    def _convert_h264_to_mp4(file_h264, file):
+    def _convert_h264_to_mp4(self,self.file_h264, self.file_mp4):
         """
         convert format h264 in mp4
         return error message if convertion is in fail
         """
 
-        command = "MP4Box -add {} {}.mp4".format(file_h264, file)
+        command = "MP4Box -add {} {}.mp4".format(self.file_h264, self.file_mp4)
         try:
             output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
         except subprocess.CalledProcessError as error:
