ea5ba39b467f3b9d1e9e6e228a09c9ac0c8ce95a
m0by314 <43465735+m0by314@users.noreply.github.com>
2019-06-09 19:07:06 +0200

    Update pir.py

--- lib/pir.py
+++ lib/pir.py
@@ -1,8 +1,30 @@
 #! /usr/bin/python3
 
 from gpiozero import MotionSensor
+import camera
 
 pir = MotionSensor(4)
 
-class Motion();
-    #TODO build class pir
+class Motion():
+    def __init__(self):
+        self.pir = MotionSensor(4)
+        self.camera = None
+        self.bot = None
+        
+    def __camera(self):
+        """
+        create camera instance
+        """
+        if self.camera is None:
+            self.camera = camera.Camera() #TODO Add args
+
+        return self.camera
+        
+    def detection(self):
+        if self.pir.motion_detected:
+            print("Motion")
+            self.camera.start_record()
+         
+       
+            
+    
