6a9578dac0e8afb9fb7f5411a4d0ea6264b01ee5
adrien <adrien>
2019-10-24 22:59:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ import sys
 
 from bin.sed import sed
 
-
 print('Welcome, the installation of the detection system will settle')
 
 if 'SUDO_USER' in os.environ:
@@ -15,51 +14,54 @@ if 'SUDO_USER' in os.environ:
 else:
     sys.exit('The setup script has not launch with sudo command')
 
-current_dir         = os.getcwd()
-script_name         = 'osiris.py'
-local_service_path  = 'etc/osiris.service'
-service_name        = 'osiris.service'
-service_path        = '/etc/systemd/system/' + service_name
-link_path           = current_dir + '/' + local_service_path
+current_dir = os.getcwd()
+prog = 'osiris.py'
+template = 'etc/osiris.service'
+service_name = 'osiris.service'
+service = '/etc/systemd/system/' + service_name
+link = current_dir + '/' + template
 
-properties = { '^ExecStart=.*'         : 'ExecStart=' + current_dir + '/' + script_name,
-                '^WorkingDirectory=.*' : 'WorkingDirectory=' + current_dir,
-                '^User=.*'             : 'User=' + user
-            }
+properties = {'^ExecStart=.*': 'ExecStart=' + current_dir + '/bin/' + prog,
+              '^WorkingDirectory=.*': 'WorkingDirectory=' + current_dir,
+              '^User=.*': 'User=' + user
+              }
 
-cmd = { "enable" : { 'args' : ["systemctl", "enable", service_name],
-                     'err'  : 'Error during enable service'
-                     },
-        "start"  : { 'args' : ["systemctl", "start", service_name],
-                     'err'  : 'Error during start service'
-                    },
-       "status"  : { 'args' : ["systemctl", "status", service_name],
-                     'err'  : 'Error during status service'
-                    },
-    }
+cmd = {"enable": {'args': ["systemctl", "enable", service_name],
+                  'err': 'Error during enable service'
+                  },
+       "start": {'args': ["systemctl", "start", service_name],
+                 'err': 'Error during start service'
+                 },
+       "status": {'args': ["systemctl", "status", service_name],
+                  'err': 'Error during status service'
+                  },
+       }
+
 
 def build_service():
-  for key in properties.keys():
-      if sed(local_service_path,key,properties[key]) != 0 :
-         raise Exception('Error during sed in' + local_service_path )
+    for key in properties.keys():
+        if sed(template, key, properties[key]) != 0:
+            raise Exception('Error during sed in' + template)
 
-def link():
-  if os.path.islink(service_path):
-      os.unlink(service_path)  
-  os.symlink(link_path, service_path)
+
+def linked():
+    if os.path.islink(service):
+        os.unlink(service)
+    os.symlink(link, service)
+
 
 def activate_service():
-  for k in cmd.keys():
-      process = subprocess.run(cmd[k]['args'])
-      if process.returncode != 0:
-         raise Exception(cmd[k]['err'])
-          
+    for k in cmd.keys():
+        process = subprocess.run(cmd[k]['args'])
+        if process.returncode != 0:
+            raise Exception(cmd[k]['err'])
+
+
 def main():
-  build_service()
-  link()
-  activate_service()
-  
-  
+    build_service()
+    linked()
+    activate_service()
+
+
 if __name__ == '__main__':
-   main()
-    
+    main()
