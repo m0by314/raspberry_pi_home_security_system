57afa14235c82819ea99593e3e13ec51f6fea0a9
Adrien <adrien.3100@gmail.com>
2021-11-03 23:17:59 +0100

    pylint error

--- app.py
+++ app.py
@@ -2,11 +2,11 @@
 """ Home surveillance application """
 import time
 
-
+from config import TOKEN_ID, REGISTRATION_FOLDER, VIDEO_TIME, CHAT_ID
 from lib.camera import Camera
 from lib.telebot import Telebot
 from lib.pir import MotionDetector
-from config import TOKEN_ID, REGISTRATION_FOLDER, VIDEO_TIME, CHAT_ID
+
 
 camera = Camera(REGISTRATION_FOLDER)
 bot = Telebot(TOKEN_ID, CHAT_ID)
