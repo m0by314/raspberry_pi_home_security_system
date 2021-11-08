abd1935650da7ff1b855bddaf6692fab02039707
Adrien <adrien.3100@gmail.com>
2021-11-08 23:42:15 +0100

    test pir

---
+++ lib/pir.py
@@ -0,0 +1,9 @@
+""" Package for interfacing with Raspberry PI PIR motion sensor. """
+ from gpiozero import MotionSensor
+
+ pir = MotionSensor(4)
+
+
+ def movement_detected() -> bool:
+     """Check if movement detected."""
+     return bool(pir.motion_detected)
